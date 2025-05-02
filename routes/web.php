847c9e6197ddd137c7ed2d0cb7803960d6478f72
Moses Muchoki <mosesmmuchoki@gmail.com>
2025-05-03 01:42:38 +0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    //return view('welcome');
+    return "<h1> Hello World!</h1>";
+});
+
+Route::get('/home', function () {
+    return view('home',['name' => "Moses"]);
+});
