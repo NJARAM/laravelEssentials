d3fec7dcb027669656f7febb8b488d5c8508ec28
Moses Muchoki <mosesmmuchoki@gmail.com>
2025-05-03 03:55:35 +0300

    make model and using ORM

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\GreetingController;
+use App\Models\student;
 
 // Route::get('/',[WelcomeController::class,'index']);
 
@@ -14,10 +15,29 @@ Route::get('/',function(){
     // dd($users);
 
     // 2. Using Query builders
-    $users=DB::table('users')->select(['name','email'])->whereNotNull('email');
-    dd($users);
+    // $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name');
+    // dd($users);
 
     //3. Using Eloquent ORM
+    //a. $students = Student::all();
+    // dd($students);
+
+    //b. $students = Student::all();
+    // foreach($students as $student){
+    //     echo $student->name."<br>";
+    // }
+
+    //c. $students = Student::select(['name','email'])->whereNotNull('email')->orderBy('name');
+    // dd($students);
+
+    //d. $students = Student::select(['name','email'])->whereNotNull('email')->orderBy('name');
+    // dd($students);
+
+    $student = new Student;
+    $student->name="jane";
+    $student->email="jane@example.com";
+    $student->save();
+    
 });
 
 Route::get('/hello',[GreetingController::class,'index']);
