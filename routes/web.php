f3b914f79e82985e37c71adf12131cc673a668d7
Moses Muchoki <mosesmmuchoki@gmail.com>
2025-05-03 03:29:18 +0300

    first db query

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\GreetingController;
 
-Route::get('/',[WelcomeController::class,'index']);
+// Route::get('/',[WelcomeController::class,'index']);
+
+Route::get('/',function(){
+    // 1.  Using raw SQL queries
+
+    // $users=DB::select('select * from users');
+    // dd($users);
+
+    // 2. Using Query builders
+    $users=DB::table('users')->select(['name','email'])->whereNotNull('email');
+    dd($users);
+
+    //3. Using Eloquent ORM
+});
 
 Route::get('/hello',[GreetingController::class,'index']);
 
